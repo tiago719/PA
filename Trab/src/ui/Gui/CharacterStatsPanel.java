4330adb4d62a6a252f6cf3725bac52f2502a6d3e
tiago719 <tiagoaacoutinho@gmail.com>
2017-06-11 02:22:37 +0100

    Grids todos feitos

--- Trab/src/ui/Gui/CharacterStatsPanel.java
+++ Trab/src/ui/Gui/CharacterStatsPanel.java
@@ -5,6 +5,8 @@
  */
 package ui.Gui;
 
+import static Logic.Constants.MaxArmor;
+import static Logic.Constants.MaxFood;
 import static Logic.Constants.MaxGold;
 import static Logic.Constants.MaxHP;
 import Logic.ObservableGame;
@@ -32,8 +34,8 @@ public class CharacterStatsPanel extends JPanel implements Constants, Observer
 {
 
     private ObservableGame observableGame;
-    private JLabel GoldStats, HpStats;
-    private ArrayList<JLabel> GoldP, HpP;
+    private JLabel GoldStats, HpStats, ArmorStats, SpellsStats, FoodStats, XpStats;
+    private ArrayList<JLabel> GoldP, HpP, ArmorP, SpellsP, FoodP, XpP;
 
     public CharacterStatsPanel(ObservableGame observableGame)
     {
@@ -42,6 +44,10 @@ public class CharacterStatsPanel extends JPanel implements Constants, Observer
 
         GoldP = new ArrayList<>();
         HpP=new ArrayList<>();
+        ArmorP=new ArrayList<>();
+        SpellsP=new ArrayList<>();
+        FoodP=new ArrayList<>();
+        XpP=new ArrayList<>();
 
         setupComponents();
         setupLayout();
@@ -51,6 +57,11 @@ public class CharacterStatsPanel extends JPanel implements Constants, Observer
     {
         GoldStats = new JLabel();
         HpStats= new JLabel();
+        ArmorStats= new JLabel();
+        SpellsStats= new JLabel();
+        FoodStats= new JLabel();
+        XpStats= new JLabel();
+        
     }
 
     public void setupLayout()
@@ -59,14 +70,56 @@ public class CharacterStatsPanel extends JPanel implements Constants, Observer
         setPreferredSize(new Dimension(DIM_X_STATS, DIM_Y_STATS));
         setMaximumSize(new Dimension(DIM_X_STATS, DIM_Y_STATS));
         setLayout(null);
+        
         GoldStats.setBounds(START_X_GOLD_PANEL, START_Y_GOLD_PANEL, DIM_X_GOLD_PANEL, DIM_Y_GOLD_PANEL);
         HpStats.setBounds(START_X_HP_PANEL, START_Y_HP_PANEL, DIM_X_HP_PANEL, DIM_Y_HP_PANEL);
+        ArmorStats.setBounds(START_X_ARMOR_PANEL, START_Y_ARMOR_PANEL, DIM_X_ARMOR_PANEL, DIM_Y_ARMOR_PANEL);
+        SpellsStats.setBounds(START_X_SPELLS_PANEL, START_Y_SPELLS_PANEL, DIM_X_SPELLS_PANEL, DIM_Y_SPELLS_PANEL);
+        FoodStats.setBounds(START_X_FOOD_PANEL, START_Y_FOOD_PANEL, DIM_X_FOOD_PANEL, DIM_Y_FOOD_PANEL);
+        XpStats.setBounds(START_X_XP_PANEL, START_Y_XP_PANEL, DIM_X_XP_PANEL, DIM_Y_XP_PANEL);
+       
         setBorder(new LineBorder(Color.GRAY));
+        
+        addXpStats();
         addGoldStats();
         addHpStats();
-
+        addArmorStats();
+        addSpellsStats();
+        addFoodStats();
+        
+        add(XpStats);
         add(GoldStats);
         add(HpStats);
+        add(ArmorStats);
+        add(SpellsStats);
+        add(FoodStats);
+    }
+    
+    public void addXpStats()
+    {
+        XpStats.setBorder(new LineBorder(Color.RED));
+
+        for (int i = 0; i < 42; i++)
+        {
+            XpP.add(new JLabel());
+            XpP.get(i).setBorder(new LineBorder(Color.BLACK));
+        }
+
+        XpStats.setLayout(new GridLayout(2, 21, DIM_X_BETWEEN, DIM_Y_BETWEEN));
+        XpStats.setVisible(true);
+
+        XpStats.setMinimumSize(new Dimension(DIM_X_XP_PANEL, DIM_Y_XP_PANEL));
+        XpStats.setPreferredSize(new Dimension(DIM_X_XP_PANEL, DIM_Y_XP_PANEL));
+        XpStats.setMaximumSize(new Dimension(DIM_X_XP_PANEL, DIM_Y_XP_PANEL));
+
+        for (int i = 0; i < XpP.size(); i++)
+        {
+            XpP.get(i).setMinimumSize(new Dimension(DIM_X_QUADRICULA, DIM_Y_QUADRICULA));
+            XpP.get(i).setPreferredSize(new Dimension(DIM_X_QUADRICULA, DIM_Y_QUADRICULA));
+            XpP.get(i).setMaximumSize(new Dimension(DIM_X_QUADRICULA, DIM_Y_QUADRICULA));
+            XpStats.add(XpP.get(i));
+
+        }
     }
 
     public void addGoldStats()
@@ -82,9 +135,6 @@ public class CharacterStatsPanel extends JPanel implements Constants, Observer
         GoldStats.setLayout(new GridLayout(1, MaxGold, DIM_X_BETWEEN, DIM_Y_BETWEEN));
         GoldStats.setVisible(true);
         
-        //GoldStats.set;
-        
-        //GoldStats.setLocation(START_X_GOLD_PANEL, START_Y_GOLD_PANEL);
         GoldStats.setMinimumSize(new Dimension(DIM_X_GOLD_PANEL, DIM_Y_GOLD_PANEL));
         GoldStats.setPreferredSize(new Dimension(DIM_X_GOLD_PANEL, DIM_Y_GOLD_PANEL));
         GoldStats.setMaximumSize(new Dimension(DIM_X_GOLD_PANEL, DIM_Y_GOLD_PANEL));
@@ -127,12 +177,93 @@ public class CharacterStatsPanel extends JPanel implements Constants, Observer
 
     }
 
+    public void addArmorStats()
+    {
+        ArmorStats.setBorder(new LineBorder(Color.RED));
+
+        for (int i = 0; i < MaxArmor; i++)
+        {
+            ArmorP.add(new JLabel());
+            ArmorP.get(i).setBorder(new LineBorder(Color.BLACK));
+        }
+
+        ArmorStats.setLayout(new GridLayout(1, MaxArmor, DIM_X_BETWEEN, DIM_Y_BETWEEN));
+        ArmorStats.setVisible(true);
+
+        ArmorStats.setMinimumSize(new Dimension(DIM_X_ARMOR_PANEL, DIM_Y_ARMOR_PANEL));
+        ArmorStats.setPreferredSize(new Dimension(DIM_X_ARMOR_PANEL, DIM_Y_ARMOR_PANEL));
+        ArmorStats.setMaximumSize(new Dimension(DIM_X_ARMOR_PANEL, DIM_Y_ARMOR_PANEL));
+
+        for (int i = 0; i < ArmorP.size(); i++)
+        {
+            ArmorP.get(i).setMinimumSize(new Dimension(DIM_X_QUADRICULA, DIM_Y_QUADRICULA));
+            ArmorP.get(i).setPreferredSize(new Dimension(DIM_X_QUADRICULA, DIM_Y_QUADRICULA));
+            ArmorP.get(i).setMaximumSize(new Dimension(DIM_X_QUADRICULA, DIM_Y_QUADRICULA));
+            ArmorStats.add(ArmorP.get(i));
+
+        }
+
+    }
+    
+    public void addSpellsStats()
+    {
+        SpellsStats.setBorder(new LineBorder(Color.RED));
+
+        for (int i = 0; i < 8; i++)
+        {
+            SpellsP.add(new JLabel());
+            SpellsP.get(i).setBorder(new LineBorder(Color.BLACK));
+        }
+
+        SpellsStats.setLayout(new GridLayout(1, 8, DIM_X_BETWEEN, DIM_Y_BETWEEN));
+        SpellsStats.setVisible(true);
+
+        SpellsStats.setMinimumSize(new Dimension(DIM_X_SPELLS_PANEL, DIM_Y_SPELLS_PANEL));
+        SpellsStats.setPreferredSize(new Dimension(DIM_X_SPELLS_PANEL, DIM_Y_SPELLS_PANEL));
+        SpellsStats.setMaximumSize(new Dimension(DIM_X_SPELLS_PANEL, DIM_Y_SPELLS_PANEL));
+
+        for (int i = 0; i < SpellsP.size(); i++)
+        {
+            SpellsP.get(i).setMinimumSize(new Dimension(DIM_X_QUADRICULA, DIM_Y_QUADRICULA));
+            SpellsP.get(i).setPreferredSize(new Dimension(DIM_X_QUADRICULA, DIM_Y_QUADRICULA));
+            SpellsP.get(i).setMaximumSize(new Dimension(DIM_X_QUADRICULA, DIM_Y_QUADRICULA));
+            SpellsStats.add(SpellsP.get(i));
+        }
+
+    }
+    
+    public void addFoodStats()
+    {
+        FoodStats.setBorder(new LineBorder(Color.RED));
+
+        for (int i = 0; i < MaxFood; i++)
+        {
+            FoodP.add(new JLabel());
+            FoodP.get(i).setBorder(new LineBorder(Color.BLACK));
+        }
+
+        FoodStats.setLayout(new GridLayout(1, MaxFood, DIM_X_BETWEEN, DIM_Y_BETWEEN));
+        FoodStats.setVisible(true);
+
+        FoodStats.setMinimumSize(new Dimension(DIM_X_FOOD_PANEL, DIM_Y_FOOD_PANEL));
+        FoodStats.setPreferredSize(new Dimension(DIM_X_FOOD_PANEL, DIM_Y_FOOD_PANEL));
+        FoodStats.setMaximumSize(new Dimension(DIM_X_FOOD_PANEL, DIM_Y_FOOD_PANEL));
+
+        for (int i = 0; i < FoodP.size(); i++)
+        {
+            FoodP.get(i).setMinimumSize(new Dimension(DIM_X_QUADRICULA, DIM_Y_QUADRICULA));
+            FoodP.get(i).setPreferredSize(new Dimension(DIM_X_QUADRICULA, DIM_Y_QUADRICULA));
+            FoodP.get(i).setMaximumSize(new Dimension(DIM_X_QUADRICULA, DIM_Y_QUADRICULA));
+            FoodStats.add(FoodP.get(i));
+        }
+
+    }
+    
     @Override
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
         g.drawImage(MiniRoguePanel.getCharacterStats(), 0, 0, DIM_X_STATS, DIM_Y_STATS, this);
-                GoldStats.setVisible(true);
 
     }
 
--- Trab/src/ui/Gui/DungeonPanel.java
+++ Trab/src/ui/Gui/DungeonPanel.java
@@ -5,12 +5,24 @@
  */
 package ui.Gui;
 
+import static Logic.Constants.MaxGold;
 import Logic.ObservableGame;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+import static ui.Gui.Constants.DIM_X_BETWEEN;
+import static ui.Gui.Constants.DIM_X_GOLD_PANEL;
+import static ui.Gui.Constants.DIM_X_QUADRICULA;
+import static ui.Gui.Constants.DIM_Y_BETWEEN;
+import static ui.Gui.Constants.DIM_Y_GOLD_PANEL;
+import static ui.Gui.Constants.DIM_Y_QUADRICULA;
 
 /**
  *
@@ -19,12 +31,17 @@ import javax.swing.JPanel;
 public class DungeonPanel extends JPanel implements Constants, Observer
 {
     private ObservableGame observableGame;
+    private JLabel AreaAtual, HpMonster;
+    private ArrayList<JLabel> AreaP, HpMonsterP;
     
     public DungeonPanel(ObservableGame observableGame)
     {
         this.observableGame=observableGame;
         
         observableGame.addObserver(this);
+        
+        AreaP = new ArrayList<>();
+        HpMonsterP=new ArrayList<>();
         
         setupComponents();
         setupLayout();
@@ -33,12 +50,79 @@ public class DungeonPanel extends JPanel implements Constants, Observer
     
     public void setupComponents()
     {
-        
+        AreaAtual = new JLabel();
+        HpMonster= new JLabel();
     }
     
     public void setupLayout()
     {
-        //setLayout(new GridLayout(5,4,DIM_X_BETWEEN, DIM_Y_BETWEEN));
+        setMinimumSize(new Dimension(DIM_X_DUNGEON, DIM_Y_DUNGEON));
+        setPreferredSize(new Dimension(DIM_X_DUNGEON, DIM_Y_DUNGEON));
+        setMaximumSize(new Dimension(DIM_X_DUNGEON, DIM_Y_DUNGEON));
+        setLayout(null);
+        
+        setBorder(new LineBorder(Color.ORANGE));
+        AreaAtual.setBounds(START_X_AREA_PANEL, START_Y_AREA_PANEL, DIM_X_AREA_PANEL, DIM_Y_AREA_PANEL);
+        HpMonster.setBounds(START_X_HPMONSTER_PANEL, START_Y_HPMONSTER_PANEL, DIM_X_HPMONSTER_PANEL, DIM_Y_HPMONSTER_PANEL);
+        
+        addAreaAtual();
+        addHpMonster();
+        
+        add(AreaAtual);
+        add(HpMonster);
+    }
+    
+    public void addHpMonster()
+    {
+        HpMonster.setBorder(new LineBorder(Color.RED));
+
+        for (int i = 0; i < MAXHPMONSTER; i++)
+        {
+            HpMonsterP.add(new JLabel());
+            HpMonsterP.get(i).setBorder(new LineBorder(Color.BLACK));
+        }
+
+        HpMonster.setLayout(new GridLayout(11, 3, DIM_X_BETWEEN_MONSTER, DIM_Y_BETWEEN_MONSTER));
+        HpMonster.setVisible(true);
+        
+        HpMonster.setMinimumSize(new Dimension(DIM_X_HPMONSTER_PANEL, DIM_Y_HPMONSTER_PANEL));
+        HpMonster.setPreferredSize(new Dimension(DIM_X_HPMONSTER_PANEL, DIM_Y_HPMONSTER_PANEL));
+        HpMonster.setMaximumSize(new Dimension(DIM_X_HPMONSTER_PANEL, DIM_Y_HPMONSTER_PANEL));
+
+        for (int i = 0; i < HpMonsterP.size(); i++)
+        {
+            HpMonsterP.get(i).setMinimumSize(new Dimension(DIM_X_QUADRICULA_MONSTER, DIM_Y_QUADRICULA_MONSTER));
+            HpMonsterP.get(i).setPreferredSize(new Dimension(DIM_X_QUADRICULA_MONSTER, DIM_Y_QUADRICULA_MONSTER));
+            HpMonsterP.get(i).setMaximumSize(new Dimension(DIM_X_QUADRICULA_MONSTER, DIM_Y_QUADRICULA_MONSTER));
+            HpMonster.add(HpMonsterP.get(i));
+
+        }
+    }
+    
+    public void addAreaAtual()
+    {
+        AreaAtual.setBorder(new LineBorder(Color.RED));
+
+        for (int i = 0; i < MAXAREA; i++)
+        {
+            AreaP.add(new JLabel());
+            AreaP.get(i).setBorder(new LineBorder(Color.BLACK));
+        }
+
+        AreaAtual.setLayout(new GridLayout(5, 4, DIM_X_BETWEEN_AREA, DIM_Y_BETWEEN_AREA));
+        AreaAtual.setVisible(true);
+        
+        AreaAtual.setMinimumSize(new Dimension(DIM_X_AREA_PANEL, DIM_Y_AREA_PANEL));
+        AreaAtual.setPreferredSize(new Dimension(DIM_X_AREA_PANEL, DIM_Y_AREA_PANEL));
+        AreaAtual.setMaximumSize(new Dimension(DIM_X_AREA_PANEL, DIM_Y_AREA_PANEL));
+
+        for (int i = 0; i < AreaP.size(); i++)
+        {
+            AreaP.get(i).setMinimumSize(new Dimension(DIM_X_QUADRICULA_AREA, DIM_Y_QUADRICULA_AREA));
+            AreaP.get(i).setPreferredSize(new Dimension(DIM_X_QUADRICULA_AREA, DIM_Y_QUADRICULA_AREA));
+            AreaP.get(i).setMaximumSize(new Dimension(DIM_X_QUADRICULA_AREA, DIM_Y_QUADRICULA_AREA));
+            AreaAtual.add(AreaP.get(i));
+        }
     }
     
     @Override
